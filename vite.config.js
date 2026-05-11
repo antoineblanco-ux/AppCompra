0cb6d72eafb3215014a9b60382e75a931b50847d
antoineblanco-ux <antoineblanco@gmail.com>
2026-05-11 22:18:35 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   server: {
-    allowedHosts: 'all'
+    allowedHosts: 'all', // Esto quita el bloqueo que ves en el móvil
+    host: true,
+    port: 5173
   }
 })
